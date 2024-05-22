4d680e66b28db31312f021faad967739d03b6238
Foucher Mathias <mathias.foucher@etu.univ-amu.fr>
2024-05-22 12:15:53 +0200

    11 pas encore totalemnt fini

--- src/main/java/fr/amu/iut/exercice11/Palette.java
+++ src/main/java/fr/amu/iut/exercice11/Palette.java
@@ -1,11 +1,15 @@
-package fr.amu.iut.exercice1;
+package fr.amu.iut.exercice11;
 
 import javafx.application.Application;
+import javafx.beans.binding.Binding;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.*;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -33,6 +37,28 @@ public class Palette extends Application {
 
     private Label texteDuBas;
 
+    private  IntegerProperty nbfois = new SimpleIntegerProperty();
+
+    private BooleanProperty pasEncoreDeClic;
+
+    public void createBindings() {
+        createPasEncoreDeClicBinding();
+        bindTextDuHaut();
+    }
+
+    private void createPasEncoreDeClicBinding() {
+        pasEncoreDeClic = new SimpleBooleanProperty();
+        pasEncoreDeClic.bind(Bindings.notEqual(0, nbfois));
+    }
+
+    private void bindTextDuHaut() {
+        Label texteDuHaut = new Label();
+        // Initial value is empty string, it changes to "Cliquez !" when pasEncoreDeClic is true
+        texteDuHaut.textProperty().bind(Bindings.when(pasEncoreDeClic)
+                .then("Cliquez !")
+                .otherwise(""));
+    }
+
 
     @Override
     public void start(Stage primaryStage) {
@@ -40,22 +66,49 @@ public class Palette extends Application {
 
         texteDuHaut = new Label();
         texteDuHaut.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        texteDuHaut.setText("Cliquez sur un boutons");
         BorderPane.setAlignment(texteDuHaut, Pos.CENTER);
 
         panneau = new Pane();
         panneau.setPrefSize(400, 200);
 
         VBox bas = new VBox();
+        nbfois = new SimpleIntegerProperty();
+        StringProperty nomDuButton = new SimpleStringProperty();
+        StringProperty couleurPanneau = new SimpleStringProperty("#000000");
         boutons = new HBox(10);
         boutons.setAlignment(Pos.CENTER);
         boutons.setPadding(new Insets(10, 5, 10, 5));
         texteDuBas = new Label();
+        texteDuBas.setText("Cliquez sur un boutons");
         bas.setAlignment(Pos.CENTER_RIGHT);
         bas.getChildren().addAll(boutons, texteDuBas);
 
         vert = new Button("Vert");
         rouge = new Button("Rouge");
         bleu = new Button("Bleu");
+
+        vert.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            nbVert ++;
+            nbfois.setValue(nbVert);
+            nomDuButton.setValue("Vert");
+            couleurPanneau.setValue("green");
+        });
+        bleu.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            nbBleu ++;
+            nbfois.setValue(nbBleu);
+            nomDuButton.setValue("Bleu");
+            couleurPanneau.setValue("blue");
+        });
+        rouge.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            nbRouge ++;
+            nbfois.setValue(nbRouge);
+            nomDuButton.setValue("Rouge");
+            couleurPanneau.setValue("red");
+        });
+
+        texteDuHaut.textProperty().bind(Bindings.concat(nomDuButton," choisi " , nbfois , " fois"));
+        panneau.styleProperty().bind(Bindings.concat("-fx-background-color : ",couleurPanneau));
 
         /* VOTRE CODE ICI */
 
@@ -71,4 +124,3 @@ public class Palette extends Application {
         primaryStage.show();
     }
 }
-
